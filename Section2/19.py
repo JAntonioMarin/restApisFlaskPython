081ecd203418367c166b41347db4917ab241f7fa
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-04-03 17:31:34 +0200

    Section 2: Loops in Python

---
+++ Section2/19.py
@@ -0,0 +1,27 @@
+number = 7
+user_input = input("Would you like to play? (Y/n): ")
+
+while user_input != "n":
+    user_number = int(input("Guess ourr number: "))
+    if user_number == number:
+        print("You guessed correctly!")
+    elif abs(number - user_number) == 1:
+        print("You were off by one.")
+    else:
+        print("sorry, it's wrong!")
+    user_input = input("Would you like to play? (Y/n): ")
+
+friends = ["Rolf", "Bob", "Jen", "Anne"]
+
+for friend in friends:
+    print(f"{friend} is my friend.")
+
+grades = [35, 67, 98, 100, 100]
+total = 0
+amount = len(grades)
+
+# total = sum(grades)
+for grade in grades:
+    total += grade
+
+print(total / amount)
