7c10cfdf0f556a88f460deb3219ba04420645700
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-04-08 18:34:24 +0200

    Section 2: Lambda functions in Pythons

---
+++ Section2/29.py
@@ -0,0 +1,14 @@
+# def add(x,y):
+#     return x + y
+
+add = lambda x, y: x + y
+
+print(add(5,7))
+
+double = lambda x: x *2
+
+sequence = [1,3,5,9]
+
+doubled = list(map(double, sequence))
+
+print(doubled)
