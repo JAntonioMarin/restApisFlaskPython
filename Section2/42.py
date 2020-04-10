70b774891efc7ae1892f411b58f571b83c875142
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-04-10 16:41:54 +0200

    Section 2: Imports in Python

---
+++ Section2/42.py
@@ -0,0 +1,9 @@
+from module42 import divide
+import sys
+
+print(divide(10,2))
+print(sys.path)
+
+# export PYTHONPATH=/Users # for export new folder path
+
+# /libs/mylib.py and __init__.py
---
+++ Section2/module42.py
@@ -0,0 +1,4 @@
+def divide(dividend, divisor):
+    return dividend / divisor
+
+print("42.py", __name__)
