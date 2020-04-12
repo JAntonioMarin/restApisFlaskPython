068c575aea6cbda65f7f8043e153d4120b22244e
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-04-12 16:20:06 +0200

    Section 2: Simple decorators in Python

---
+++ Section2/47.py
@@ -0,0 +1,27 @@
+user = {"username": "jose", "access_level": "guest"}
+# user = {"username": "jose", "access_level": "admin"}
+
+def get_admin_password():
+    return "1234"
+
+def secure_get_admin():
+    if user["access_level"] == "admin":
+        return "1234"
+
+def secure_function(func):
+    if user["access_level"] == "admin":
+        return func
+
+def make_secure(func):
+    def secure_function():
+        if user["access_level"] == "admin":
+            return func()
+        else:
+            return f"No admin permissions for {user['username']}."
+
+    return secure_function()
+
+
+get_admin_password = make_secure(get_admin_password)
+
+print(get_admin_password)
