f18e93a7971319fce4c5d96a54b936d2bf5221a8
Juan Antonio Marin Sanchez <avalanch.psp@gmail.com>
2020-04-17 20:06:43 +0200

    Section 4: Virtualenvs and setting up Flask-RESTful

---
+++ Section4/68.py
@@ -0,0 +1,19 @@
+# pip3.8 freeze
+#
+# click==7.1.1
+# Flask==1.1.2
+# itsdangerous==1.1.0
+# Jinja2==2.11.1
+# MarkupSafe==1.1.1
+# Werkzeug==1.0.1
+
+# Create virtual environment, u can use pip3 or your version of pip
+# pip3.8 install virtualenv
+
+# For activate
+# source venv/bin/activate
+# pip3.8 install Flask-RESTful
+
+# deactivate for quit venv
+
+print('Use virtualenv!!!')
