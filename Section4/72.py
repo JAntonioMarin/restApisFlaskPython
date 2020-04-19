fa7f97fc20ad7289ce53fd093cc94fbafb343cc1
JAntonioMarin <avalanch.psp@gmail.com>
2020-04-19 12:52:08 +0200

    Section 4: Creating our item Resource

---
+++ Section4/72.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'name': None}, 404
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201  # 201 CREATED
+
+
+api.add_resource(Item, '/item/<string:name>')  # http://127.0.0.1:5000/item/
+
+app.run(port=5000)
