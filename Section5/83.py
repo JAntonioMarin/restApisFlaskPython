ee07aa27d1ffacb644105a0101dfe1785d23afbf
JAntonioMarin <avalanch.psp@gmail.com>
2020-04-21 18:59:53 +0200

    Section 5:Setting up out project

---
+++ Section5/83.py
@@ -0,0 +1,7 @@
+# Create new env and install dependencies
+# virtualenv venv --python=python3.8
+# virtualenv venv --python=python3.5
+# source venv/bin/activate
+# pip3.8 install Flask
+# pip3.8 install Flask-RESTful
+# pip3.8 install Flask-JWT
