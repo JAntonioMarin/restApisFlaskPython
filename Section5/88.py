2b3ad34aad6450bf472b75c4e672372f4327f938
JAntonioMarin <avalanch.psp@gmail.com>
2020-04-25 11:55:35 +0200

    Section 5: Preventing duplicate usernames when signing users up

--- Section5/88.py
+++ Section5/88.py
@@ -1,4 +1,4 @@
 # user.py
 
-if user.find_by_username(data['username']):
-    return {"message": "A user with that username already exists"}, 400
+# if user.find_by_username(data['username']):
+#     return {"message": "A user with that username already exists"}, 400
