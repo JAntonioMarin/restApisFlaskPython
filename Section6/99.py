69125297794d1e938b9a8b042a5d7251e94d9b1b
JAntonioMarin <avalanch.psp@gmail.com>
2020-04-27 19:29:31 +0200

    Section 6: Setting up this section's project

---
+++ Section6/99.py
@@ -0,0 +1,7 @@
+# virtualenv venv --python=python3.8
+# source venv/bin/activate
+# pip3 install Flask
+# pip3 install Flask-JWT
+# pip3 install Flask-RESTful
+# pip3 install Flask-SQLAlchemy
+# copy files of section5 for modify
