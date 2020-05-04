7b6a25baed3bbcce6d48f3594b3db04b5e101a4f
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-04 18:36:41 +0200

    Section 6: Creating a new model: StoreModel

--- Section6/resources/item.py
+++ Section6/resources/item.py
@@ -14,6 +14,11 @@ class Item(Resource):
         required=True,
         help="This field cannot be left blank!"
     )
+    parser.add_argument('store_id',
+        type=int,
+        required=True,
+        help="Every item needs a store id"
+    )
 
     # @jwt_required()
     def get(self, name):
@@ -28,7 +33,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
@@ -52,7 +57,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
 
