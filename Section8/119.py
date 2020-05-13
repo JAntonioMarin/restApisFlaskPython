9061e44aaea771b640aeea41bf65ec750cff0b5e
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-14 00:23:03 +0200

    Section 8: Working with Git and automatic deploys

---
+++ Section8/119.py
@@ -0,0 +1,10 @@
+# Enable automatic deploy in Heroku under branch master
+# git branch feature/item-categories
+# git checkout feature/item-categories
+# Add new code
+# git add newcode.py
+# git commit -m "Add new code"
+# git push --set-upstream origin feature/item-categories
+# git checkout master
+# git merge feature/item-categories
+# git push origin master
