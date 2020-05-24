fda36a9ebd9b6c270145715fdaa10333dba34116
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-24 19:12:01 +0200

    Section 11: Performing token refresh in our REST API

--- Section11/resources/user.py
+++ Section11/resources/user.py
@@ -1,4 +1,4 @@
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 
@@ -66,3 +66,11 @@ class UserLogin(Resource):
 
         return {'message': 'Invalid credentials'}, 401
         # create access token
+
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_toke': new_token}, 200
