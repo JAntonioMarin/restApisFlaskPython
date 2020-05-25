a586d575c93953ff4fb1ee8576a8cffaf68515fc
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-25 19:59:41 +0200

    Section 11: Blacklisting with Flask-JWT-Extended

--- Section11/app.py
+++ Section11/app.py
@@ -6,11 +6,15 @@ from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
 app.secret_key = 'jose' # app.config['JWT_SECRET_KEY'] = 'jose'
 api = Api(app)
 
@@ -26,6 +30,10 @@ def add_claims_to_jwt(identity):
     if identity == 1:
         return {'is_admin': True}
     return {'is_admin': False}
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST
 
 @jwt.expired_token_loader
 def expired_token_loader():
