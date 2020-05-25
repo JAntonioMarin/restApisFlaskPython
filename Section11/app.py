ccf08105165a51525ea506dcd5190e3b4eca2730
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-25 20:25:33 +0200

    Section 11: Logging users out in out REST API

--- Section11/app.py
+++ Section11/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
-from resources.user import UserRegister, User, UserLogin, TokenRefresh
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
@@ -77,6 +77,8 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(StoreList, '/stores')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
+
 api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
--- Section11/resources/user.py
+++ Section11/resources/user.py
@@ -1,8 +1,10 @@
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity, \
+    jwt_required
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 
 from models.user import UserModel
+from blacklist import BLACKLIST
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username',
@@ -67,6 +69,13 @@ class UserLogin(Resource):
         return {'message': 'Invalid credentials'}, 401
         # create access token
 
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        from flask_jwt_extended import get_raw_jwt
+        jti = get_raw_jwt()['jti']
+        BLACKLIST.add(jti)
+        return {'message': "Sucessfully"}
 
 class TokenRefresh(Resource):
     @jwt_refresh_token_required
